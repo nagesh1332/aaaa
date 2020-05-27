72c3c6a23a089680beb250f4ba78db972ed503cc
nagesh1332 <48804135+nagesh1332@users.noreply.github.com>
2020-05-27 17:12:31 +0530

    four

--- abc.py
+++ abc.py
@@ -114,14 +114,10 @@ if final_accuracy < 0.92:
 else:
     print("Your New accuracy=",final_accuracy)
 final_accuracy=history.history["val_accuracy"][-1]
-	print(final_accuracy)
+    print(final_accuracy)
 	
 
-	import os
-	if final_accuracy < 0.92:
-	    os.system("curl --user '<jenkins user>:<jenkins password>' http://172.20.10.4:8080/L/job/job3/build?token=retrain")
-	else:
-	    print("Your New accuracy=",final_accuracy)
+	
 
 
 
